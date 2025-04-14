0a25417ba0f0a0f132540eb94d346ebcc2e71af3
chrisOkeith01 <christopherkeith70@gmail.com>
2025-04-14 18:04:00 +0000

    star

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
-
+	const [planet,setPlanet]=useState(null)
+	useEffect(()=>{
+		const getPlanet = async()=>{
+			const response = await fetch("https://www.swapi.tech/api/planets/1/")
+			const data =  await response.json()
+			setPlanet(data.result.properties)
+			
+		}
+		getPlanet()
+		
+	},[])
+	
   const {store, dispatch} =useGlobalReducer()
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Home</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				{planet ?(
+				<p>Planet Name:{planet.name}</p>	
+				):(
+					<p>loading</p>
+				)}
+				
 			</p>
 		</div>
 	);
